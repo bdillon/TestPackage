81af2e70da4624e658b2814011c8dc3228e6b527
Brian Dillon <brianwdillon@gmail.com>
2015-06-12 10:13:44 -0400

    Minor change
    Minor change - BWD

--- R/Sample.R
+++ R/Sample.R
@@ -1,8 +1,8 @@
-#' A function to print "Hi Wing-Yee and Ming!".
+#' A function to print "Hi Wing Yee and Ming!".
 #'
 #' @param words A vector of strings that represents a tokenized document
 #' @export
 
 HelloWorld <- function() {
-    cat("Hello world!")
+    cat("Hello Wing Yee and Ming!")
 }
