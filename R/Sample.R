afec2e6df41082389f73d2eed389b6dee2533a2e
Brian Dillon <brianwdillon@gmail.com>
2015-06-12 10:03:59 -0400

    Sample.R added
    Added a new sample function - BWD

---
+++ R/Sample.R
@@ -0,0 +1,8 @@
+#' A function to print "Hi Wing-Yee and Ming!".
+#'
+#' @param words A vector of strings that represents a tokenized document
+#' @export
+
+HelloWorld <- function() {
+    cat("Hello world!")
+}
